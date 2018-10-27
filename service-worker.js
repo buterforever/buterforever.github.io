ec113c7f9395a2b2b16b58f03141e7c459af8ff9
Александр Будников <aleksandr@iMac-Aleksandr.local>
2018-10-27 14:13:17 +0800

    New service Worker

--- service-worker.js
+++ service-worker.js
@@ -77,42 +77,98 @@ self.addEventListener('activate', event => {
   );
 });
 
+var config = {
+  version: 'achilles',
+  staticCacheItems: [
+    '/index.html',
+    '/bmw.jpg',
+    '/'
+  ],
+  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
+    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
+    + '<title id="offline-title">Offline</title>'
+    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
+    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
+    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlinePage: '/offline/'
+};
+
+function cacheName (key, opts) {
+  return `${opts.version}-${key}`;
+}
+
+function addToCache (cacheKey, request, response) {
+  if (response.ok) {
+    var copy = response.clone();
+    caches.open(cacheKey).then( cache => {
+      cache.put(request, copy);
+    });
+  }
+  return response;
+}
+
+function fetchFromCache (event) {
+    return caches.match(event.request).then(response => {
+      if (!response) {
+      throw Error(`${event.request.url} not found in cache`);
+    }
+    return response;
+  });
+}
+
+function offlineResponse (resourceType, opts) {
+  if (resourceType === 'image') {
+  return new Response(opts.offlineImage,
+        { headers: { 'Content-Type': 'image/svg+xml' } }
+      );
+    } else if (resourceType === 'content') {
+  return caches.match(opts.offlinePage);
+    }
+  return undefined;
+}
+
 self.addEventListener('fetch', (event) => {
   function shouldHandleFetch (event, opts) {
     if (event.request.url.startsWith(self.location.origin)) return true;
     return false;
   }
 
-  function onFetch (event/*, opts*/) {
-    console.log('Обрабатываем запрос на наш сервис и кешируем данные');
-    return caches.open(cacheName).then(function(cache) {
-      cache.put(event.request, response.clone());
-      return response;
-    });
+  function onFetch (event, opts) {
+    console.log('Запустили функцию onFetch');
+    var request = event.request;
+    var acceptHeader = request.headers.get('Accept');
+    var resourceType = 'static';
+    var cacheKey;
+
+    if (acceptHeader.indexOf('text/html') !== -1) {
+      resourceType = 'content';
+    } else if (acceptHeader.indexOf('image') !== -1) {
+      resourceType = 'image';
+    }
+    cacheKey = cacheName(resourceType, opts);
+
+    if (resourceType === 'content') {
+      event.respondWith(
+      fetch(request)
+        .then(response => addToCache(cacheKey, request, response))
+        .catch(() => fetchFromCache(event))
+        .catch(() => offlineResponse(resourceType, opts))
+      );
+    } else {
+      event.respondWith(
+        fetchFromCache(event)
+          .catch(() => fetch(request))
+          .then(response => addToCache(cacheKey, request, response))
+          .catch(() => offlineResponse(resourceType, opts))
+      );
+    }
   }
 
-  if (shouldHandleFetch(event/*, config*/)) {
-    onFetch(event/*, config*/);
+  if (shouldHandleFetch(event, config)) {
+    onFetch(event, config);
   } else{
     console.log('А тут мы отправляем запрос как обычно, потому что это другой сервер');
     console.log(event.request.url);
   }
-
-  /*event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        if (event.request.url.startsWith(self.location.origin)) {
-          console.log('если мы обращаемся к своему серверу, то добавляем запрос в кеш');
-          return caches.open('v1').then(function(cache) {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        } else{
-          console.log('иначе, если это чужой сервер, то просто возвращаем запрос');
-          console.log(response);
-          return response;
-        }
-      });
-    })
-  );*/
 });
