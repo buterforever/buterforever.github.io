47394c7e346f2378c94992c1bba66a512bc5461f
Александр Будников <aleksandr@iMac-Aleksandr.local>
2018-10-29 16:13:23 +0800

    cache fix

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,21 @@
 'use strict';
 
-
-let version_cache = 'vers3';
-//let cacheName = version_cache+'_cache';
+var config = {
+  version: 'achilles4',
+  staticCacheItems: [
+    '/index.html',
+    '/bmw.jpg',
+    '/'
+  ],
+  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
+    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
+    + '<title id="offline-title">Offline</title>'
+    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
+    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
+    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlinePage: '/offline/'
+};
 
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
@@ -76,23 +89,6 @@ self.addEventListener('activate', event => {
      .then( () => self.clients.claim() )
   );
 });
-
-var config = {
-  version: 'achilles3',
-  staticCacheItems: [
-    '/index.html',
-    '/bmw.jpg',
-    '/'
-  ],
-  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
-  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
-    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
-    + '<title id="offline-title">Offline</title>'
-    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
-    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
-    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
-  offlinePage: '/offline/'
-};
 
 function cacheName (key, opts) {
   return opts.version+'-'+key;
